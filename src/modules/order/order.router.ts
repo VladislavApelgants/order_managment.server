81b1c4c714596e92a987375aefa9548f0e38abc1
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2025-02-15 10:33:24 +0200

    fix order schema

--- src/modules/order/order.router.ts
+++ src/modules/order/order.router.ts
@@ -2,12 +2,10 @@ import { Router } from 'express';
 import {createOrderController} from "./order.controller";
 import {validateBody} from "../../middlewares";
 import {ctrlWrapper} from "../../utils/ctrlWrapper";
-import {createOrderSchema} from "./validation/orderShema";
-
+import {createOrderSchema} from "./order.validation";
 
 const router = Router();
 
 router.post('/', validateBody(createOrderSchema), ctrlWrapper(createOrderController));
-
 
 export default router;
